24d0890857ed56dc175dd35d568b9d89bfe361e6
mrzalais <lejnieksedijs@gmail.com>
2023-07-09 10:41:58 +0300

    Refactor code to create new response

--- app/Middleware/ValidationExceptionMiddleware.php
+++ app/Middleware/ValidationExceptionMiddleware.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Middleware;
 
 use App\Exception\ValidationException;
+use Psr\Http\Message\ResponseFactoryInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
@@ -12,14 +13,20 @@ use Psr\Http\Server\RequestHandlerInterface;
 
 class ValidationExceptionMiddleware implements MiddlewareInterface
 {
+    public function __construct(private readonly ResponseFactoryInterface $responseFactory)
+    {
+    }
+
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
         try {
-            $response = $handler->handle($request);
+            return $handler->handle($request);
         } catch (ValidationException $exception) {
+            $response = $this->responseFactory->createResponse();
 
+            $referer = $request->getServerParams()['HTTP_REFERER'];
+
+            return $response->withHeader('Location', $referer)->withStatus(302);
         }
-
-
     }
 }
