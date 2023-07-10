f616ede395a8e04a3fa13debff7428bdeab9e236
mrzalais <lejnieksedijs@gmail.com>
2023-07-10 11:28:15 +0300

    Add error flashing and form data keep on fail

--- app/Middleware/ValidationExceptionMiddleware.php
+++ app/Middleware/ValidationExceptionMiddleware.php
@@ -24,8 +24,12 @@ class ValidationExceptionMiddleware implements MiddlewareInterface
         } catch (ValidationException $exception) {
             $response = $this->responseFactory->createResponse();
             $referer = $request->getServerParams()['HTTP_REFERER'];
+            $oldData = $request->getParsedBody();
+
+            $sensitiveFields = ['password', 'confirmPassword'];
 
             $_SESSION['errors'] = $exception->errors;
+            $_SESSION['old'] = array_diff_key($oldData, array_flip($sensitiveFields));
 
             return $response->withHeader('Location', $referer)->withStatus(302);
         }
