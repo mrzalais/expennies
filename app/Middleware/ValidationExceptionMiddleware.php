b526f5731cfb31bf64f37984bb264e13c1d78be7
mrzalais <lejnieksedijs@gmail.com>
2023-07-14 08:59:28 +0300

    Refactor flash messages to session class

--- app/Middleware/ValidationExceptionMiddleware.php
+++ app/Middleware/ValidationExceptionMiddleware.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Middleware;
 
+use App\Contracts\SessionInterface;
 use App\Exception\ValidationException;
 use Psr\Http\Message\ResponseFactoryInterface;
 use Psr\Http\Message\ResponseInterface;
@@ -13,8 +14,10 @@ use Psr\Http\Server\RequestHandlerInterface;
 
 class ValidationExceptionMiddleware implements MiddlewareInterface
 {
-    public function __construct(private readonly ResponseFactoryInterface $responseFactory)
-    {
+    public function __construct(
+        private readonly ResponseFactoryInterface $responseFactory,
+        private readonly SessionInterface $session
+    ) {
     }
 
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
@@ -28,8 +31,8 @@ class ValidationExceptionMiddleware implements MiddlewareInterface
 
             $sensitiveFields = ['password', 'confirmPassword'];
 
-            $_SESSION['errors'] = $exception->errors;
-            $_SESSION['old'] = array_diff_key($oldData, array_flip($sensitiveFields));
+            $this->session->flash('errors', $exception->errors);
+            $this->session->flash('old', array_diff_key($oldData, array_flip($sensitiveFields)));
 
             return $response->withHeader('Location', $referer)->withStatus(302);
         }
