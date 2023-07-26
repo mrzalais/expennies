6a95e0ed3f24e198bb56881fb2d93a8f2b3904b3
mrzalais <lejnieksedijs@gmail.com>
2023-07-26 23:00:52 +0300

    Add file upload

--- app/Controllers/ReceiptController.php
+++ app/Controllers/ReceiptController.php
@@ -4,16 +4,25 @@ declare(strict_types = 1);
 
 namespace App\Controllers;
 
+use League\Flysystem\Filesystem;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\UploadedFileInterface;
 
 class ReceiptController
 {
+    public function __construct(private readonly Filesystem $filesystem)
+    {
+    }
+
     public function store(Request $request, Response $response, array $args): Response
     {
         /** @var UploadedFileInterface $file */
         $file = $request->getUploadedFiles()['receipt'];
+
+        $fileName = $file->getClientFilename();
+
+        $this->filesystem->write('receipts/' . $fileName, $file->getStream()->getContents());
 
         return $response;
     }
