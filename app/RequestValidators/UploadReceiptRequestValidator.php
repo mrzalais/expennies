dc1784e7ce5ad9370f457ed980c02e4c4248acdc
mrzalais <lejnieksedijs@gmail.com>
2023-08-02 23:38:53 +0300

    Use flysystem mime type detection

--- app/RequestValidators/UploadReceiptRequestValidator.php
+++ app/RequestValidators/UploadReceiptRequestValidator.php
@@ -6,7 +6,7 @@ namespace App\RequestValidators;
 
 use App\Contracts\RequestValidatorInterface;
 use App\Exception\ValidationException;
-use finfo;
+use League\MimeTypeDetection\FinfoMimeTypeDetector;
 use Psr\Http\Message\UploadedFileInterface;
 
 class UploadReceiptRequestValidator implements RequestValidatorInterface
@@ -37,31 +37,19 @@ class UploadReceiptRequestValidator implements RequestValidatorInterface
         }
 
         $allowedMimeTypes = ['image/jpeg', 'image/png', 'application/pdf'];
-        $allowedExtensions = ['pdf', 'png', 'jpeg', 'jpg'];
         $tmpFilePath = $uploadedFile->getStream()->getMetadata('uri');
 
         if (!in_array($uploadedFile->getClientMediaType(), $allowedMimeTypes)) {
             throw new ValidationException(['receipt' => ['Receipt has to be either an image or a pdf document']]);
         }
 
-        if (!in_array($this->getExtension($tmpFilePath), $allowedExtensions)) {
-            throw new ValidationException(['receipt' => ['Receipt has to be either pdf, png, jpg, or jpeg']]);
-        }
+        $detector = new FinfoMimeTypeDetector();
+        $mimeType = $detector->detectMimeType($tmpFilePath, $uploadedFile->getStream()->getContents());
 
-        if (!in_array($this->getMimeType($tmpFilePath), $allowedMimeTypes)) {
+        if (!in_array($mimeType, $allowedMimeTypes)) {
             throw new ValidationException(['receipt' => ['Invalid file type']]);
         }
 
         return $data;
-    }
-
-    private function getExtension(string $path): string
-    {
-        return (new finfo(FILEINFO_EXTENSION))->file($path) ?: '';
-    }
-
-    private function getMimeType(string $path): string
-    {
-        return (new finfo(FILEINFO_MIME_TYPE))->file($path) ?: '';
     }
 }
