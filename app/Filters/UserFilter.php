5ec756f079d1c7c5ba9db170c98118ce736fffd5
mrzalais <lejnieksedijs@gmail.com>
2023-08-10 22:17:17 +0300

    Add interface for entities with relationship to user

--- app/Filters/UserFilter.php
+++ app/Filters/UserFilter.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Filters;
 
+use App\Contracts\OwnableInterface;
 use Doctrine\ORM\Mapping\ClassMetadata;
 use Doctrine\ORM\Query\Filter\SQLFilter;
 
@@ -11,6 +12,9 @@ class UserFilter extends SQLFilter
 {
     public function addFilterConstraint(ClassMetadata $targetEntity, $targetTableAlias): string
     {
+        if (!$targetEntity->getReflectionClass()->implementsInterface(OwnableInterface::class)) {
+            return '';
+        }
         return $targetTableAlias . '.user_id = ' . $this->getParameter('user_id');
     }
 }
