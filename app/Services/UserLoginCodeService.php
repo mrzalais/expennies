e5eb9dd7e37e7124301ab1d3c18eb4364254e572
mrzalais <lejnieksedijs@gmail.com>
2023-08-11 21:44:40 +0300

    Implement 2FA code through email

---
+++ app/Services/UserLoginCodeService.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Services;
+
+use App\Contracts\EntityManagerServiceInterface;
+use App\Entity\User;
+use App\Entity\UserLoginCode;
+use DateTime;
+
+class UserLoginCodeService
+{
+    public function __construct(private readonly EntityManagerServiceInterface $entityManager)
+    {
+    }
+
+    public function generate(User $user): UserLoginCode
+    {
+        $userLoginCode = new UserLoginCode();
+
+        $code = random_int(100000, 999999);
+
+        $userLoginCode->setCode((string) $code);
+        $userLoginCode->setExpiration(new DateTime('+10 minutes'));
+        $userLoginCode->setUser($user);
+
+        $this->entityManager->sync($userLoginCode);
+
+        return $userLoginCode;
+    }
+}
