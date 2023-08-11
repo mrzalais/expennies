68ffb89dbe709f1f92b25f2dd4acdb7cad20c8e1
mrzalais <lejnieksedijs@gmail.com>
2023-08-11 10:43:55 +0300

    Implement verify method

--- app/Controllers/VerifyController.php
+++ app/Controllers/VerifyController.php
@@ -4,14 +4,19 @@ declare(strict_types=1);
 
 namespace App\Controllers;
 
+use App\Entity\User;
+use App\Services\UserProviderService;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use RuntimeException;
 use Slim\Views\Twig;
 
 class VerifyController
 {
-    public function __construct(private readonly Twig $twig)
-    {
+    public function __construct(
+        private readonly Twig $twig,
+        private readonly UserProviderService $userProviderService
+    ) {
     }
 
     public function index(ResponseInterface $response): ResponseInterface
@@ -19,8 +24,19 @@ class VerifyController
         return $this->twig->render($response, 'auth/verify.twig');
     }
 
-    public function verify(ServerRequestInterface $request, ResponseInterface $response): ResponseInterface
+    public function verify(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
     {
-        return $response;
+        /** @var User $user */
+        $user = $request->getAttribute('user');
+
+        if (! hash_equals((string) $user->getId(), $args['id']) || ! hash_equals(sha1($user->getEmail()), $args['hash'])) {
+            throw new RuntimeException('Verification failed');
+        }
+
+        if (!$user->getVerifiedAt()) {
+            $this->userProviderService->verifyUser($user);
+        }
+
+        return $response->withHeader('Location', '/')->withStatus(302);
     }
 }
