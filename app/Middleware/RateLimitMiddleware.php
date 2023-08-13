b268a0b33da9acd785a37131c9880f84df5694ef
mrzalais <lejnieksedijs@gmail.com>
2023-08-13 14:40:32 +0300

    Implement symfony rate limiter

--- app/Middleware/RateLimitMiddleware.php
+++ app/Middleware/RateLimitMiddleware.php
@@ -4,30 +4,34 @@ declare(strict_types=1);
 
 namespace App\Middleware;
 
+use App\Config;
+use App\Services\RequestService;
 use Psr\Http\Message\ResponseFactoryInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
-use Psr\SimpleCache\CacheInterface;
+use Slim\Routing\RouteContext;
+use Symfony\Component\RateLimiter\RateLimiterFactory;
 
 class RateLimitMiddleware implements MiddlewareInterface
 {
     public function __construct(
-        private readonly CacheInterface $cache,
-        private readonly ResponseFactoryInterface $responseFactory
+        private readonly ResponseFactoryInterface $responseFactory,
+        private readonly RequestService $requestService,
+        private readonly Config $config,
+        private readonly RateLimiterFactory $rateLimitFactory
     ) {
     }
 
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
-        $clientIp = $request->getServerParams()['REMOTE_ADDR'];
-        $cacheKey = 'rate_limit_' . $clientIp;
-        $requests = (int) $this->cache->get($cacheKey);
+        $clientIp = $this->requestService->getClientIp($request, $this->config->get('trusted_proxies'));
+        $routeContext = RouteContext::fromRequest($request);
+        $route = $routeContext->getRoute();
+        $limiter = $this->rateLimitFactory->create($route->getName() . '_' . $clientIp);
 
-        $this->cache->set($cacheKey, $requests + 1, 60);
-
-        if ($requests > 3) {
+        if ($limiter->consume()->isAccepted() === false) {
             return $this->responseFactory->createResponse(429, 'Too many requests');
         }
 
--- configs/container/container_bindings.php
+++ configs/container/container_bindings.php
@@ -47,6 +47,8 @@ use Symfony\Component\Mailer\Mailer;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mailer\Transport;
 use Symfony\Component\Mime\BodyRendererInterface;
+use Symfony\Component\RateLimiter\RateLimiterFactory;
+use Symfony\Component\RateLimiter\Storage\CacheStorage;
 use Symfony\WebpackEncoreBundle\Asset\EntrypointLookup;
 use Symfony\WebpackEncoreBundle\Asset\TagRenderer;
 use Symfony\WebpackEncoreBundle\Twig\EntryFilesTwigExtension;
@@ -162,15 +164,22 @@ return [
     },
     BodyRendererInterface::class            => fn(Twig $twig) => new BodyRenderer($twig->getEnvironment()),
     RouteParserInterface::class             => fn(App $app) => $app->getRouteCollector()->getRouteParser(),
-    CacheInterface::class                   => function (Config $config) {
+    CacheInterface::class                   => fn (RedisAdapter $redisAdapter) => new Psr16Cache($redisAdapter),
+    RedisAdapter::class => function(Config $config) {
         $redis  = new \Redis();
         $config = $config->get('redis');
 
         $redis->connect($config['host'], (int) $config['port']);
         $redis->auth($config['password']);
 
-        $adapter = new RedisAdapter($redis);
-
-        return new Psr16Cache($adapter);
+        return new RedisAdapter($redis);
     },
+    RateLimiterFactory::class => function(RedisAdapter $redisAdapter) {
+        $storage = new CacheStorage($redisAdapter);
+
+        return new RateLimiterFactory(
+            ['id' => 'default', 'policy' => 'fixed_window', 'interval' => '1 minute', 'limit' => 3],
+            $storage
+        );
+    }
 ];
